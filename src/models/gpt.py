f94812106678634e9215131774ccd39bebf8945b
Kuleen Sasse <cometku@gmail.com>
2023-03-13 11:32:07 -0400

    created untested model classes

---
+++ src/models/gpt.py
@@ -0,0 +1,58 @@
+import os
+
+import logging
+
+from typing import Iterable, List 
+
+import openai
+import tqdm
+
+logger = logging.getLogger(__name__ +'.models')
+logging.getLogger("openai").setLevel(logging.WARNING)
+
+
+class GPT:
+    def __init__(self, model_name: str, batch_size: int = 32):
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.model_name = model_name
+        self.batch_size = batch_size    
+        
+    def get_response(self, prompt: Iterable[str], temperature=0):
+        response = openai.Completion.create(model=self.model_name, 
+                                            prompt=prompt, 
+                                            temperature=temperature,
+                                            max_tokens=100
+                                            )
+        return response
+
+    def format_response(self, response):
+        text = response['text'].replace('\n', ' ').strip()
+        return text
+
+    def generate_from_prompts(self, examples: Iterable[str], fout_path:str) -> List[str]:
+        lines_length = len(examples)
+        logger.info(f'Num examples = {lines_length}')
+        i = 0
+
+        responses =  []
+
+        for i in tqdm(range(0, lines_length, self.batch_size), ncols=0):
+            prompt_batch = examples[i:min(i+self.batch_size, lines_length)]
+            try:
+                response = self.get_response(prompt_batch)
+                for line in response['choices']:
+                    line = self.format_response(line)
+                    responses.append(line + '\n')
+            except:
+                for i in range(len(prompt_batch)):
+                    try:
+                        _r = self.get_response(prompt_batch[i])['choices'][0]
+                        line = self.format_response(_r)
+                        responses.append(line + '\n')
+                    except:
+                        l_prompt = len(prompt_batch[i])
+                        _r = self.get_response(prompt_batch[i][l_prompt-2000:])['choices'][0]
+                        line = self.format_response(_r)
+                        responses.append(line + '\n')
+
+        return responses
---
+++ src/models/t5.py
@@ -0,0 +1,43 @@
+import json
+
+import os
+
+import requests
+
+from typing import Iterable, List 
+
+import tqdm
+
+
+
+class T5:
+    def __init__(self, model_url: str, batch_size: int = 32):
+        self.api_key = os.getenv("HF_ACCESS_TOKEN")
+        self.model_url = model_url
+        self.batch_size = batch_size
+        self.headers = {"Authorization": f"Bearer {self.api_key}"}
+
+    def get_response(self, prompt: str, temperature=0):
+
+        payload = json.dumps({"inputs": prompt, "parameters": {"temperature" : temperature}})
+
+        response = requests.post(self.model_url, headers=self.headers, json=payload)
+
+        return json.loads(response.content.decode("utf-8"))
+
+    def format_response(self, response):
+        text = response['text'].replace('\n', ' ').strip()
+        return text
+
+    def generate_from_prompts(self, examples: Iterable[str]) -> List[str]:
+        responses =  []
+
+        for example in tqdm(examples):
+            try:
+                response = self.get_response(example)['generated_text']
+                formatted_response = self.format_response(response)
+                responses.append(formatted_response + '\n')
+            except:
+                continue
+
+        return responses
