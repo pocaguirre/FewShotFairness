4bb6c83553b9c539164b3a17d5b4b2a9e0df6309
Kuleen Sasse <cometku@gmail.com>
2023-03-19 17:40:43 -0400

    updated chatgpt to remove roles updated gpt to index its outputs cleaned up huggingface api

--- src/models/gpt.py
+++ src/models/gpt.py
@@ -2,57 +2,63 @@ import os
 
 import logging
 
-from typing import Iterable, List 
+from typing import Iterable, List
 
 import openai
-from tqdm import tqdm 
+from tqdm import tqdm
 
-logger = logging.getLogger(__name__ +'.models')
+logger = logging.getLogger(__name__ + ".models")
 logging.getLogger("openai").setLevel(logging.WARNING)
 
 
 class GPT:
     def __init__(self, model_name: str, batch_size: int = 32):
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.model_name = model_name
-        self.batch_size = batch_size    
         
-    def get_response(self, prompt: Iterable[str], temperature=0):
-        response = openai.Completion.create(model=self.model_name, 
-                                            prompt=prompt, 
-                                            temperature=temperature,
-                                            max_tokens=1
-                                            )
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        self.model_name = model_name
+        self.batch_size = batch_size
+
+    def get_response(self, prompt: Iterable[str], temperature=1):
+        response = openai.Completion.create(
+            model=self.model_name, prompt=prompt, temperature=temperature, max_tokens=15
+        )
         return response
 
     def format_response(self, response):
-        text = response['text'].replace('\n', ' ').strip()
+        text = response["text"].replace("\n", " ").strip()
         return text
 
     def generate_from_prompts(self, examples: Iterable[str]) -> List[str]:
         lines_length = len(examples)
-        logger.info(f'Num examples = {lines_length}')
+        logger.info(f"Num examples = {lines_length}")
         i = 0
 
-        responses =  []
+        responses = []
 
         for i in tqdm(range(0, lines_length, self.batch_size), ncols=0):
-            prompt_batch = examples[i:min(i+self.batch_size, lines_length)]
+            prompt_batch = examples[i : min(i + self.batch_size, lines_length)]
             try:
                 response = self.get_response(prompt_batch)
-                for line in response['choices']:
-                    line = self.format_response(line)
-                    responses.append(line + '\n')
+
+                response_batch = [""] * len(prompt_batch)
+
+                for choice in response.choices:
+                    response_batch[choice.index] = self.format_response(choice.text)+ "\n"
+
+                responses.extend(response_batch)
+
             except:
                 for i in range(len(prompt_batch)):
                     try:
-                        _r = self.get_response(prompt_batch[i])['choices'][0]
+                        _r = self.get_response(prompt_batch[i])["choices"][0]
                         line = self.format_response(_r)
-                        responses.append(line + '\n')
+                        responses.append(line + "\n")
                     except:
                         l_prompt = len(prompt_batch[i])
-                        _r = self.get_response(prompt_batch[i][l_prompt-2000:])['choices'][0]
+                        _r = self.get_response(prompt_batch[i][l_prompt - 2000 :])[
+                            "choices"
+                        ][0]
                         line = self.format_response(_r)
-                        responses.append(line + '\n')
+                        responses.append(line + "\n")
 
         return responses
--- src/models/hf.py
+++ src/models/hf.py
@@ -4,9 +4,10 @@ import os
 
 import requests
 
-from typing import Iterable, List 
+from typing import Iterable, List
 
-import tqdm
+from tqdm import tqdm
+
 
 class HF:
     def __init__(self, model_url: str):
@@ -15,25 +16,26 @@ class HF:
         self.headers = {"Authorization": f"Bearer {self.api_key}"}
 
     def get_response(self, prompt: str, temperature=0):
-
-        payload = json.dumps({"inputs": prompt, "parameters": {"temperature" : temperature}})
+        payload = json.dumps(
+            {"inputs": prompt, "parameters": {"temperature": temperature}}
+        )
 
         response = requests.post(self.model_url, headers=self.headers, json=payload)
 
         return json.loads(response.content.decode("utf-8"))
 
     def format_response(self, response):
-        text = response['text'].replace('\n', ' ').strip()
+        text = response["text"].replace("\n", " ").strip()
         return text
 
     def generate_from_prompts(self, examples: Iterable[str]) -> List[str]:
-        responses =  []
+        responses = []
 
         for example in tqdm(examples):
             try:
-                response = self.get_response(example)['generated_text']
+                response = self.get_response(example)["generated_text"]
                 formatted_response = self.format_response(response)
-                responses.append(formatted_response + '\n')
+                responses.append(formatted_response + "\n")
             except:
                 continue
 
