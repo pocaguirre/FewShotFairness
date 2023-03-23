48df96342018a2890244a9673868a0fbd7193abf
Kuleen Sasse <cometku@gmail.com>
2023-03-23 16:36:22 -0400

    added comments to gpt model

--- src/models/gpt.py
+++ src/models/gpt.py
@@ -14,6 +14,9 @@ logging.getLogger("openai").setLevel(logging.WARNING)
 
 
 class GPT(APIModel):
+    """Code modified from
+    https://github.com/isabelcachola/generative-prompting/blob/main/genprompt/models.py
+    """
     def __init__(self, model_name: str, temperature: float = 1, max_tokens: int = 5):
 
         super.__init__(model_name, temperature, max_tokens)
@@ -47,29 +50,36 @@ class GPT(APIModel):
 
         responses = []
 
+        
         for i in tqdm(range(0, lines_length, self.batch_size), ncols=0):
+
+            # batch prompts together
             prompt_batch = examples[i : min(i + self.batch_size, lines_length)]
             try:
+                # try to get respones
                 response = self.get_response(prompt_batch)
 
                 response_batch = [""] * len(prompt_batch)
 
+                # order the responses as they are async
                 for choice in response.choices:
                     response_batch[choice.index] = self.format_response(choice.text)
 
                 responses.extend(response_batch)
 
+            # catch any connection exceptions
             except:
+
+                # try each prompt individually
                 for i in range(len(prompt_batch)):
                     try:
                         _r = self.get_response(prompt_batch[i])["choices"][0]
                         line = self.format_response(_r)
                         responses.append(line)
                     except:
+                        # if there is an exception make blank
                         l_prompt = len(prompt_batch[i])
-                        _r = self.get_response(prompt_batch[i][l_prompt - 2000 :])[
-                            "choices"
-                        ][0]
+                        _r = self.get_response(prompt_batch[i][l_prompt - 2000 :])["choices"][0]
                         line = self.format_response(_r)
                         responses.append(line)
 
