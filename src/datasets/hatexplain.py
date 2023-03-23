e704959c2bd3abf676c9e06d8931fe8ad7e12ff0
Kuleen Sasse <cometku@gmail.com>
2023-03-23 14:35:41 -0400

    added comments and docstrings

--- src/datasets/hatexplain.py
+++ src/datasets/hatexplain.py
@@ -1,6 +1,6 @@
 import os
 
-from typing import Tuple, List
+from typing import Tuple, List, Optional, Any, Dict
 
 import json
 
@@ -10,46 +10,76 @@ from collections import Counter
 
 
 class HatExplain(Dataset):
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
+        """Wrapper for HatExplain Dataset
+
+        :param path: path to HatExplain dataset
+        :type path: str
+        :raises ValueError: path does not exist
+        """        
         super().__init__(path)
 
-        self.datasets = {}
+        self.datasets: Dict[str, Any] = {}
 
         if not os.path.exists(self.path):
             raise ValueError(f"Path to HateExplain data: {self.path} does not exist")
+        
+        # read json 
+        data: Dict[str, Any] = json.load(open(os.path.join(self.path, "dataset.json"), "r"))
 
-        data = json.load(open(os.path.join(self.path, "dataset.json"), "r"))
+        # read splits
+        splits: Dict[str, List[str]] = json.load(open(os.path.join(self.path, "post_id_divisions.json"), "r"))
 
-        splits = json.load(open(os.path.join(self.path, "post_id_divisions.json"), "r"))
-
+        # split data into train and test
         self.datasets["train"] = [data[x] for x in splits["train"]]
         self.datasets["test"] = [data[x] for x in splits["test"]]
 
     def build_prompt(self, text: str, label: str) -> str:
         return text + " \n " + label
 
-    def get_majority(self, lst: List[str]) -> str:
-        """
+    def get_majority(self, lst: List[str]) -> Optional[str]:
+        """Helper to get majority of a list
+        If there is no majority returns none
+        
         Using method from 
         https://github.com/INK-USC/CrossFit/blob/master/tasks/hatexplain.py
-        """
+
+        :param lst: list of items to count
+        :type lst: List[str]
+        :return: returns highest counted item or none if there is a tie
+        :rtype: Optional[str]
+        """        
         c = Counter(lst)
         rank = c.most_common()
+
+        # if there is only one item return the item
         if len(rank) == 1:
             return rank[0][0]
+        
+        # if there is a tie return none
         elif rank[0][1] == rank[1][1]:
             return None
+        
+        # return the majority
         else:
             return rank[0][0]
 
     def create_prompts(self) -> Tuple[List[str], List[str], List[str], List[List[str]]]:
+        """Creates prompts for HateExplain
+
+        :return: returns the train and test prompts, the labels for the test set and the demographic groups of the test set
+        :rtype: Tuple[List[str], List[str], List[str], List[List[str]]]
+        """        
         train_prompts = []
 
+        # create train prompts
         for item in self.datasets["train"]:
             labels = [x["label"] for x in item["annotators"]]
 
+            # get majority label
             label = self.get_majority(labels)
 
+            # if there is no majority label we remove it
             if label is not None:
                 if label == "hatespeech":
                     label = "hate speech"
@@ -66,10 +96,14 @@ class HatExplain(Dataset):
 
         test_demographics = []
 
+        # create test prompts
         for item in self.datasets["test"]:
             labels = [x["label"] for x in item["annotators"]]
 
-            item_demographics = list(set([x['label'] for x in item['annotators']]))
+            item_demographics = [x['target'] for x in item['annotators']]
+
+            # get all the demographics associated with the item 
+            item_demographics = list(set([element for sublist in item_demographics for element in sublist]))
 
             label = self.get_majority(labels)
 
--- src/demonstrations/random.py
+++ src/demonstrations/random.py
@@ -5,17 +5,27 @@ from typing import List, Iterable, Tuple
 from tqdm import tqdm
 
 class RandomSampler:
-    
-    def __init__(self, shots = 16):
+    def __init__(self, shots=16) -> None:
+
         self.shots = shots
-    
-    def create_demonstrations(self, train_set: Iterable[str], test_set: Iterable[str]) -> List[str]:
-        
+
+    def create_demonstrations(
+        self, train_set: Iterable[str], test_set: Iterable[str]
+    ) -> List[str]:
+        """Create random k-shot from train set and test set
+
+        :param train_set: list of train prompts
+        :type train_set: Iterable[str]
+        :param test_set: list of test prompts
+        :type test_set: Iterable[str]
+        :return: k-shot demonstrations for each test set item 
+        :rtype: List[str]
+        """        
         demonstrations = []
 
         for item in tqdm(test_set):
             train_dems = random.sample(train_set, self.shots)
 
-            demonstrations.append(" \n".join(train_dems) + item)
-        
+            demonstrations.append("\n\n".join(train_dems) + "\n\n" + item)
+
         return demonstrations
