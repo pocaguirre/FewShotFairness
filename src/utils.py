8fd53a6f15a777f424bd0ffecabffb592192b919
Kuleen Sasse <cometku@gmail.com>
2023-03-31 10:30:00 -0400

    formatted utils

--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,16 @@ from sklearn.metrics import f1_score, confusion_matrix
 
 import copy
 
-def metrics(preds: List[str], labels: List[str], dataset: str, demographics: List[List[str]]) -> Dict[str, Any]:
-    """Returns a dictionary of overall macro f1 score, 
-    recall, specificity, and macro f1 for each group 
+
+def metrics(
+    preds: List[str],
+    labels: List[str],
+    dataset: str,
+    demographics: List[List[str]],
+    overall_demographics: List[str],
+) -> Dict[str, Any]:
+    """Returns a dictionary of overall macro f1 score,
+    recall, specificity, and macro f1 for each group
     and largest gap in recall for each label
 
     Code heavily modified from https://github.com/pocaguirre/jiant/blob/master/jiant/ext/fairness/DF_training.py
@@ -20,36 +27,22 @@ def metrics(preds: List[str], labels: List[str], dataset: str, demographics: Lis
     :type dataset: str
     :param demographics: list of lists of demographic groups associated with labels
     :type demographics: List[List[str]]
-    :return: dictionary of overall macro f1 score, recall, specificity, and macro f1 for each group 
+    :param overall_demographics: Demographics to focus on in the output
+    :type overall_demographics: List[str]
+    :return: dictionary of overall macro f1 score, recall, specificity, and macro f1 for each group
     and largest gap in recall for each label
     :rtype: Dict[str, Any]
-    """    
+    """
 
-    scores = {
-        "recall": {},
-        "specificity": {},
-        "score" : {}
-    }
+    scores = {"recall": {}, "specificity": {}, "score": {}}
 
     # create subgroups to focus on
-    demographic_groups = None
-
-    # hatexplain requires filtering of certain classes to focus on
-    if dataset == "hatexplain-race":
-        demographic_groups = ["African", "Arab", "Asian", "Hispanic", "Caucasian", "Indian", "Indigenous"]
-
-    elif dataset == "hatexplain-gender":
-        demographic_groups = ["Men", "Women"]
-
-    else:
-        demographic_groups = list(set([element for sublist in demographics for element in sublist]))  
-
     preds_clean = copy.deepcopy(preds)
 
     # clean up predictions
     preds_clean = [x.lower() for x in preds_clean]
 
-    conv = lambda i : i or ''
+    conv = lambda i: i or ""
     preds_clean = [conv(i) for i in preds_clean]
 
     if dataset == "bias":
@@ -67,9 +60,9 @@ def metrics(preds: List[str], labels: List[str], dataset: str, demographics: Lis
     dummy_preds = []
 
     for pred in preds_clean:
-        
+
         # see if any of the labels are in the response
-        for label in labels_set:            
+        for label in labels_set:
             if pred.find(label) != -1:
                 dummy_preds.append(labels_dict[label])
                 break
@@ -80,9 +73,13 @@ def metrics(preds: List[str], labels: List[str], dataset: str, demographics: Lis
     dummy_preds = np.array(dummy_preds)
     dummy_labels = np.array(dummy_labels)
 
-    # remove predictions that have demographics not in the set 
+    # remove predictions that have demographics not in the set
     # mostly for hatexplain which has multiple demographics per label
-    demographic_index = [i for i, item in enumerate(demographics) if len(set(demographic_groups).intersection(set(item))) != 0 ]
+    demographic_index = [
+        i
+        for i, item in enumerate(demographics)
+        if len(set(overall_demographics).intersection(set(item))) != 0
+    ]
 
     demographics_filtered = copy.deepcopy([demographics[i] for i in demographic_index])
 
@@ -90,19 +87,23 @@ def metrics(preds: List[str], labels: List[str], dataset: str, demographics: Lis
     dummy_labels = dummy_labels[demographic_index]
 
     # get total score
-    scores['total_score'] = f1_score(dummy_labels, dummy_preds, average="macro", labels = list(labels_dict.values()))
+    scores["total_score"] = f1_score(
+        dummy_labels, dummy_preds, average="macro", labels=list(labels_dict.values())
+    )
 
-    for dem in demographic_groups:
+    for dem in overall_demographics:
 
         # filter out items that do not have the specified demographic
         index = [i for i, item in enumerate(demographics_filtered) if dem in item]
 
         # calculate f1, recall and specifity for those items
-        cnf_matrix = confusion_matrix(dummy_labels[index], dummy_preds[index], labels = list(labels_dict.values()))
+        cnf_matrix = confusion_matrix(
+            dummy_labels[index], dummy_preds[index], labels=list(labels_dict.values())
+        )
 
         fn = cnf_matrix.sum(axis=1) - np.diag(cnf_matrix)
         tp = np.diag(cnf_matrix)
-        fp = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix) 
+        fp = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix)
         tn = cnf_matrix.sum() - (fp + fn + tp)
 
         fn = fn.astype(float)
@@ -110,26 +111,30 @@ def metrics(preds: List[str], labels: List[str], dataset: str, demographics: Lis
         fp = fp.astype(float)
         tn = tn.astype(float)
 
-        score = f1_score(dummy_labels[index], dummy_preds[index], average="macro", labels = list(labels_dict.values()))
+        score = f1_score(
+            dummy_labels[index],
+            dummy_preds[index],
+            average="macro",
+            labels=list(labels_dict.values()),
+        )
 
         recall = tp / (tp + fn)
         specificity = tn / (tn + fp)
 
-
-        scores['recall'][dem] = recall
+        scores["recall"][dem] = recall
         scores["specificity"][dem] = specificity
         scores["score"][dem] = score
 
     gaps = []
 
     # calculate all the TPR gaps for every possible combination of demographics
-    for group1 in scores['recall']:
-        for group2 in scores['recall']:
-            gap =  scores['recall'][group1] - scores['recall'][group2]
+    for group1 in scores["recall"]:
+        for group2 in scores["recall"]:
+            gap = scores["recall"][group1] - scores["recall"][group2]
 
             gap = np.nan_to_num(gap)
 
-            one_minus_gap = 1-gap
+            one_minus_gap = 1 - gap
             gaps.append([group1, group2, gap, one_minus_gap])
 
     # get the maximum TPR gap per class
